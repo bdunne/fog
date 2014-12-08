96f114631f23769d64eac9dce2b5b6cbc1534638
Brandon Dunne <bdunne@redhat.com>
2014-12-08 12:21:56 -0500

    HACK: Get test suite running again

--- tests/helper.rb
+++ tests/helper.rb
@@ -2,7 +2,7 @@ ENV['FOG_RC']         = ENV['FOG_RC'] || File.expand_path('../.fog', __FILE__)
 ENV['FOG_CREDENTIAL'] = ENV['FOG_CREDENTIAL'] || 'default'
 
 require 'fog'
-require 'fog/core/bin' # for available_providers and registered_providers
+# require 'fog/core/bin' # for available_providers and registered_providers
 
 Excon.defaults.merge!(:debug_request => true, :debug_response => true)
 
@@ -25,26 +25,26 @@ def array_differences(array_a, array_b)
   (array_a - array_b) | (array_b - array_a)
 end
 
-# check to see which credentials are available and add others to the skipped tags list
-all_providers = Fog.registered_providers.map {|provider| provider.downcase}
+# # check to see which credentials are available and add others to the skipped tags list
+# all_providers = Fog.registered_providers.map {|provider| provider.downcase}
 
-# Manually remove these providers since they are local applications, not lacking credentials
-all_providers = all_providers - ["libvirt", "openvz"]
+# # Manually remove these providers since they are local applications, not lacking credentials
+# all_providers = all_providers - ["libvirt", "openvz"]
 
-available_providers = Fog.available_providers.map {|provider| provider.downcase}
+# available_providers = Fog.available_providers.map {|provider| provider.downcase}
 
-unavailable_providers = all_providers - available_providers
+# unavailable_providers = all_providers - available_providers
 
-if !ENV['PROVIDER'].nil? && unavailable_providers.include?(ENV['PROVIDER'])
-  Formatador.display_line("[red]Requested provider #{ENV['PROVIDER']} is not available.[/]" +
-                          "[red]Check if .fog file has correct configuration (see '#{Fog.credentials_path}')[/]")
-  exit(0)
-end
+# if !ENV['PROVIDER'].nil? && unavailable_providers.include?(ENV['PROVIDER'])
+#   Formatador.display_line("[red]Requested provider #{ENV['PROVIDER']} is not available.[/]" +
+#                           "[red]Check if .fog file has correct configuration (see '#{Fog.credentials_path}')[/]")
+#   exit(0)
+# end
 
-for provider in unavailable_providers
-  Formatador.display_line("[yellow]Skipping tests for [bold]#{provider}[/] [yellow]due to lacking credentials (add some to '#{Fog.credentials_path}' to run them)[/]")
-  Thread.current[:tags] << ('-' << provider)
-end
+# for provider in unavailable_providers
+#   Formatador.display_line("[yellow]Skipping tests for [bold]#{provider}[/] [yellow]due to lacking credentials (add some to '#{Fog.credentials_path}' to run them)[/]")
+#   Thread.current[:tags] << ('-' << provider)
+# end
 
 # mark libvirt tests pending if not setup
 begin
